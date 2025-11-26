db4918e9005b3608e6af1b4e02ad57e719c5df3e
eliezer-software-enginner <eliezerassuncaocustodio@gmail.com>
2025-11-26 09:47:37 -0300

    added channel

--- app/src/main/java/my/app/ChannelsHandler.java
+++ app/src/main/java/my/app/ChannelsHandler.java
@@ -7,7 +7,9 @@ public class ChannelsHandler {
     public List<Channel> loadChannels() {
         return List.of(
                 new Channel("Teste","-1003457993247"),
-                new Channel("Apk livre","-1003406204965"));
+                new Channel("Apk livre","-1003406204965"),
+                new Channel("G1 - Noticias Oficiais do g1", "-1002403342784")
+                );
     }
     //salvar canais
     //carregar canais
